ce1e65db108e0b2c0000ebd7fac6d3dba730c7b1
Kapelushny <zig.tos@gmail.com>
2019-06-24 13:08:23 +0300

    chore sc

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,27 +1,41 @@
 package tacos;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.hibernate.validator.constraints.NotBlank;
 
 import lombok.Data;
 
-//tag::newFields[]
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+
+  private static final long serialVersionUID = 1L;
   
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
   
   private Date placedAt;
   
-//end::newFields[]
-
+  @ManyToOne(targetEntity = User.class)
+  private User user;
+  
   @NotBlank(message="Delivery name is required")
   private String deliveryName;
   
@@ -30,8 +44,8 @@ public class Order {
   
   @NotBlank(message="City is required")
   private String deliveryCity;
-
-  @Size(message="State is required 2 chars", min=2, max=2)
+  
+  @NotBlank(message="State is required")
   private String deliveryState;
   
   @NotBlank(message="Zip code is required")
@@ -47,19 +61,16 @@ public class Order {
   @Digits(integer=3, fraction=0, message="Invalid CVV")
   private String ccCVV;
 
+  @ManyToMany(targetEntity=Taco.class)
   private List<Taco> tacos = new ArrayList<>();
   
   public void addDesign(Taco design) {
     this.tacos.add(design);
   }
   
-  /*
-// tag::newFields[]
-  ...
-
-// end::newFields[]
-   */
-//tag::newFields[]
+  @PrePersist
+  void placedAt() {
+    this.placedAt = new Date();
+  }
+  
 }
-//end::newFields[]
-
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,34 +2,32 @@ package tacos;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
 
-// tag::newFields[]
 @Data
+@Entity
 public class Taco {
 
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
   
-  private Date createdAt;
-
-//end::newFields[]
-
   @NotNull
   @Size(min=5, message="Name must be at least 5 characters long")
   private String name;
   
+  private Date createdAt;
+
+  @ManyToMany(targetEntity=Ingredient.class)
   @Size(min=1, message="You must choose at least 1 ingredient")
   private List<Ingredient> ingredients;
 
-  /*
-//tag::newFields[]
-   ...
-   
-//end::newFields[]
-   */
-//tag::newFields[]
+  @PrePersist
+  void createdAt() {
+    this.createdAt = new Date();
+  }
 }
-//end::newFields[]
