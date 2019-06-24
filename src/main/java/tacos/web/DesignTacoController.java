bca3a294e896013c2c9f212dded39c48549080fa
Kapelushny <zig.tos@gmail.com>
2019-06-24 12:18:10 +0300

    add spring data jdbc, h2 db etc.

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,101 +1,121 @@
 package tacos.web;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
-@Slf4j
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// tag::classShell[]
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+  
+//end::classShell[]
 
+//tag::bothRepoProperties[]
+//tag::ingredientRepoProperty[]
+  private final IngredientRepository ingredientRepo;
+  
+//end::ingredientRepoProperty[]
+  private TacoRepository designRepo;
 
-    @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+//end::bothRepoProperties[]
+  
+  /*
+// tag::ingredientRepoOnlyCtor[]
+  @Autowired
+  public DesignTacoController(IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
+// end::ingredientRepoOnlyCtor[]
+   */
 
-        Type[] types = Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
-        }
+  //tag::bothRepoCtor[]
+  @Autowired
+  public DesignTacoController(
+        IngredientRepository ingredientRepo, 
+        TacoRepository designRepo) {
+    this.ingredientRepo = ingredientRepo;
+    this.designRepo = designRepo;
+  }
+
+  //end::bothRepoCtor[]
+  
+  // tag::modelAttributes[]
+  @ModelAttribute(name = "order")
+  public Order order() {
+    return new Order();
+  }
+  
+  @ModelAttribute(name = "taco")
+  public Taco taco() {
+    return new Taco();
+  }
+
+  // end::modelAttributes[]
+  // tag::showDesignForm[]
+  
+  @GetMapping
+  public String showDesignForm(Model model) {
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+    
+    Type[] types = Type.values();
+    for (Type type : types) {
+      model.addAttribute(type.toString().toLowerCase(), 
+          filterByType(ingredients, type));      
     }
 
-    //tag::showDesignForm[]
-    @GetMapping
-    public String showDesignForm(Model model) {
-        model.addAttribute("design", new Taco());
-        return "design";
-    }
-
+    return "design";
+  }
 //end::showDesignForm[]
 
-/*
-//tag::processDesign[]
+  //tag::processDesign[]
   @PostMapping
-  public String processDesign(Design design) {
-    // Save the taco design...
-    // We'll do this in chapter 3
-    log.info("Processing design: " + design);
+  public String processDesign(
+      @Valid Taco design, Errors errors, 
+      @ModelAttribute Order order) {
+
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
+    Taco saved = designRepo.save(design);
+    order.addDesign(saved);
 
     return "redirect:/orders/current";
   }
+  //end::processDesign[]
+  
+  private List<Ingredient> filterByType(
+      List<Ingredient> ingredients, Type type) {
+    return ingredients
+              .stream()
+              .filter(x -> x.getType().equals(type))
+              .collect(Collectors.toList());
+  }
 
-//end::processDesign[]
- */
+  /*
+//tag::classShell[]
 
-    //tag::processDesignValidated[]
-    @PostMapping
-    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            return "design";
-        }
+  ...
 
-        // Save the taco design...
-        // We'll do this in chapter 3
-        log.info("Processing design: " + design);
+//end::classShell[]
+   */
+//tag::classShell[]
 
-        return "redirect:/orders/current";
-    }
-
-//end::processDesignValidated[]
-
-    //tag::filterByType[]
-    private List<Ingredient> filterByType(
-            List<Ingredient> ingredients, Type type) {
-        return ingredients
-                .stream()
-                .filter(x -> x.getType().equals(type))
-                .collect(Collectors.toList());
-    }
-
-//end::filterByType[]
-// tag::foot[]
 }
-// end::foot[]
+//end::classShell[]
